eab59e2f5a1a63d93a9174d8cb0aef18690520e3
Felipe Pinto <felipe14062000@gmail.com>
2026-04-03 16:39:01 -0300

    feat: add home page

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <div>
+      <h1>Template</h1>
+      <h2>Tecnologias Utilizadas</h2>
+      <ul>
+        <li>Next</li>
+        <li>PG</li>
+        <li>Node-pg-migrate</li>
+        <li>Jest</li>
+        <li>Husky</li>
+        <li>Eslint</li>
+        <li>Prettier</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
