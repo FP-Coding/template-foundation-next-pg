1c2f6b2886949847361f3e815edd0ca11e35b7ca
Felipe Pinto <felipe14062000@gmail.com>
2026-04-03 18:49:36 -0300

    chore: install and configurate jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import prettier from "eslint-config-prettier/flat";
+import jest from "eslint-plugin-jest";
 import js from "@eslint/js";
+import globals from "globals";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   prettier,
+  {
+    files: ["**/*.test.js", "**/*.spec.js"],
+    languageOptions: {
+      globals: globals.jest,
+    },
+    plugins: { jest },
+    rules: { ...jest.configs.recommended.rules },
+  },
   { files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
   globalIgnores([".next/**", ".env.*"]),
 ]);
---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+const nextJest = require("next/jest");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: ".env.development" });
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config = {
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  testTimeout: 60000,
+};
+
+module.exports = createJestConfig(config);
