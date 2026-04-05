5ece43a994fa3624b363cee756da48a0515df913
Felipe Pinto <felipe14062000@gmail.com>
2026-04-04 22:01:50 -0300

    feat: create database.js

---
+++ infra/database.js
@@ -0,0 +1,43 @@
+import { Client } from "pg";
+
+const getNewClient = async () => {
+  const clientConfig = {
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    ssl: getSSL(),
+  };
+
+  const client = new Client(clientConfig);
+
+  await client.connect();
+
+  return client;
+};
+
+const query = async (queryObject) => {
+  let client;
+  try {
+    client = await getNewClient();
+    const resultQuery = await client.query(queryObject);
+    return resultQuery;
+  } catch (err) {
+    throw new Error(err);
+  } finally {
+    await client.end();
+  }
+};
+
+const getSSL = () => {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production";
+};
+
+export { getNewClient, query };
